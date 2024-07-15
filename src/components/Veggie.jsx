999c9a0951e48a938f6403c7802c55c4b6267dbf
LorddBonee <oluwayemi_e@yahoo.com>
2024-07-15 15:26:31 +0100

    first commit

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -22,7 +22,7 @@ function Veggie() {
         if(check){
             setVeggie(JSON.parse(check))
         } else {
-            const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`);
+            const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&include-tags=vegetarian`);
             const data = await api.json();
 
             localStorage.setItem('popular', JSON.stringify(data.recipes))
